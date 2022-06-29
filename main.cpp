f1293c7880153049227d7ac856b74fad2493b6b0
vlShkapych <42435621+vlShkapych@users.noreply.github.com>
2022-06-29 20:28:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 
+#include "ThisThread.h"
 #include "mbed.h"
-#include "Manchester.h"
-#include <cstdio>
-#include <string>
 
 int main()
 {
    
    
+    printf("sempai");
+    static Manchester man(PB_2,PB_3);
 
-    static Manchester man(PB_2,PB_3,9600);
-
-   
+    printf("sempai");
     string name = "Vlad Shkapych";
 
     for(int i = 0; i < name.length(); i++) {
-        man.write(name[i]);
-        printf("d");   
+        man.write(name[i],9600);
+          
     } 
+    ThisThread::sleep_for(1);
+
     for(int i = 0; i < name.length(); i++) {
         
         printf("%c",man.read());
@@ -27,3 +27,4 @@ int main()
         
 
 }
+
